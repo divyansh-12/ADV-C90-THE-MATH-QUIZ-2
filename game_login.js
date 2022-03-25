33cc3b435af85603d0e763ae64a49fac4077700b
divyansh-12 <89969868+divyansh-12@users.noreply.github.com>
2022-03-25 09:07:46 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,6 @@
+addUser()
+{
+    window.location = "quiz_game_page.html";
+    localStorage.setItem("player-1_name_input", player-1_name_input);
+    localStorage.setItem("player-2_name_input", player-2_name_input);
+}
